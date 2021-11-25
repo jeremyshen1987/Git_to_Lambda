3984d20d40dee968903cfd2c37690345f21d7b6e
Jeremy Shen <jeremyshen1987@gmail.com>
2021-11-24 22:36:10 -0700

    test

--- lambda_function.py
+++ lambda_function.py
@@ -2,5 +2,5 @@ def lambda_handler(event, context):
     message = 'Hello {} {}!'.format(event['First_Name'], event['Last_Name'])  
     return { 
         'message' : message
-        print('Done!')
+      
     }
